3f1a2409582198ee3db5e4887e45c073ce19f90c
echimine <eliott.chiminello@gmail.com>
2025-04-01 16:00:23 +0200

    added header

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
-<link rel="stylesheet" href="style.css">
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/1dc9ffc1cd.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./pages/header/style.css">
     <title>Document</title>
 </head>
+
 <body>
+    <?php
 
+    include('./pages/header/header.php');
+
+    ?>
+
+    <?php
+
+    include('./pages/login.php');
+    include('./pages/main.php');
+    include('./pages/detailsPost.php');
+    ?>
 </body>
+
 </html>
-
-<?php
-
-include('./pages/login.php');
-include('./pages/main.php');
-include('./pages/detailsPost.php');
---
+++ pages/header/header.php
@@ -0,0 +1,23 @@
+<header>
+    <div>
+        <ul>
+            <li>
+                ECHIMINE
+            </li>
+            <nav>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        Articles
+                    </li>
+                </ul>
+            </nav>
+            <li>
+                <i class="fa-regular fa-circle-user"></i>
+            </li>
+        </ul>
+    </div>
+
+</header>
