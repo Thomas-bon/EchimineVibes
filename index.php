2934acac3ebcb700b03062cba7d53a113cc3cb4f
nathan74490 <na.boudet@formation-cci.fr>
2025-04-01 16:01:07 +0200

    add footer

--- index.php
+++ index.php
@@ -3,16 +3,29 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="footer.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+<?php
 
+if (isset($_GET["page"]) && $_GET["page"] == "login") {
+    include "pages/login.php";
+} elseif (isset($_GET["page"]) && $_GET["page"] == "register") {
+    include "pages/register.php";
+} else {
+    include('./pages/main.php');
+}
+ 
+?>
+
+<footer>
+    <?php
+    include('./pages/footer.php');
+    ?>
+</footer>
 </body>
 </html>
 
-<?php
-
-include('./pages/login.php');
-include('./pages/main.php');
-include('./pages/detailsPost.php');
