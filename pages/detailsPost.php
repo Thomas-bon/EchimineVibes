8fda88f87aa898e17e804c81827bcc171a0e8ec7
Thomas-bon <pro.bonanseathomas@gmail.com>
2025-04-02 14:11:51 +0200

    Added change detailPost

--- pages/detailsPost.php
+++ pages/detailsPost.php
@@ -1,10 +1,10 @@
 <?php
-
-
+ 
+ 
 $id_user = $_SESSION['user'];
-
-
-
+ 
+ 
+ 
 $idArticle = $_GET['id'];
 // if (!$connection) { //Si la connexion n'a pas été effectué
 //     die("Connection impossible");
@@ -13,15 +13,26 @@ $idArticle = $_GET['id'];
     echo "<div>";
     while ($resultat = mysqli_fetch_array($requete)) {
         // $link = '/EchimineVibes/pages/detailsPost/?post=' . $resultat['id_article'];
-        echo '<div class = articles>'. '<h1>'.$resultat['title'].'</h1>'.'<div class=articlesContent>'.$resultat['content'].'</div>'.'<img class=articlesImage src= ../'.$resultat['img_article'].'>' .'</div>' . '<br>';
+        echo '<div class = articles>'. '<h1>'.$resultat['title'].'</h1>'.'<div class=articlesContent>'.$resultat['content'].'</div>' .'</div>' . '<br>';
     }
     echo "</div>";
-
-
+ 
+    $requeteComm = mysqli_query($connection, "
+    SELECT blog_comments.content, blog_user.user_pseudo
+    FROM blog_comments
+    INNER JOIN blog_user ON blog_comments.id_user = blog_user.id_user
+    WHERE blog_comments.id_article = $idArticle
+");
+ 
+while ($resultat = mysqli_fetch_array($requeteComm)) {
+    echo "<p>Commentaire de " . $resultat["user_pseudo"] . " : " . $resultat["content"] . "</p>";
+}
+ 
+ 
     echo "<div>";
     echo '<form action="/EchimineVibes/BDD_Functions/comments_fonctions.php" method="POST">';
     echo '    <label for="texte">Votre texte :</label>';
-    echo '    <input type="text" name="texte" id="texte" required>'; 
+    echo '    <input type="text" name="texte" id="texte" required>';
     echo '    <input type="hidden" value="' . htmlspecialchars($id_user) . '" id="id_user" name="id_user">';
     echo '    <input type="hidden" value="' . htmlspecialchars($idArticle) . '" id="idArticle" name="idArticle">';
     echo '    <button type="submit">Envoyer</button>';
